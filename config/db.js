2f7f4c070cd181e13ac295d7d22384f5a4c6b7b5
Ritu Raj Shandilya <riturajyo147147@gmail.com>
2021-12-13 20:31:02 +0530

    dep4 commit

--- config/db.js
+++ config/db.js
@@ -2,14 +2,18 @@ require("dotenv").config()
 const mongoose=require("mongoose")
 
 function connectDB() {
-  // Database connection 🥳
-  mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
-  const connection = mongoose.connection;
-  connection.once('open', () => {
-      console.log('Database connected 🥳🥳🥳🥳');
-  }).catch(err => {
-      console.log('Connection failed ☹️☹️☹️☹️');
-  });
+  mongoose.connect(
+    process.env.MONGO_CONNECTION_URL, 
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  );
+  const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
 }
 
 // mIAY0a6u1ByJsWWZ
